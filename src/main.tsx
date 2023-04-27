ec14cda6ffe15314a822cb87aa2a2e386fb4bde3
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-04-27 10:20:55 +0700

    ColorMode Switch

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import router from "./router.tsx";
+import theme from "./theme.ts";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
+      <ColorModeScript initialColorMode={theme.config.initialColorMode} />
       <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>

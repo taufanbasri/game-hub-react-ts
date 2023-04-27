195ecfed51ed7c560efea238b34e31b45c90c641
Taufan Abu Nada <taufanprasetyobasri@gmail.com>
2023-04-27 23:03:21 +0700

    Add static platform list

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,4 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import platforms from "../data/platforms";
 import Platform from "../entities/Platform";
 import APIClient from "../services/api-client";
 
@@ -6,7 +8,9 @@ const apiClient = new APIClient<Platform>('/platforms/lists/parents')
 
 const usePlatforms = () => useQuery({
   queryKey: ['platforms'],
-  queryFn: apiClient.getAll
+  queryFn: apiClient.getAll,
+  staleTime: ms('24h'),
+  initialData: platforms
 })
 
 export default usePlatforms
